ebbf4db841abb1cd449b5c22131625a4c2b0b964
tomoya.harada <freiheitlibre0420@icloud.com>
2019-01-18 10:21:55 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+ 
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "tasks#index"
+  root to: "toppages#index"
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   
   resources :tasks
-
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :new, :create]
 end
