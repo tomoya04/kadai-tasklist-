e2d1cb0565f41fe0295f18697d16c61022e5fefa
tomoya.harada <freiheitlibre0420@icloud.com>
2019-01-19 06:24:48 +0000

    login post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
     
     def index
      @tasks = Task.order(created_at: :desc).page(params[:page]).per(10)
@@ -9,19 +11,17 @@ class TasksController < ApplicationController
       
     end
     
-    def new
-      @task = Task.new
-    end
+    
      
     def create
-      @task = Task.new(task_params)
-      
+     @task = current_user.tasks.build(task_params)
       if @task.save
-      flash[:success] = "Taskが正常に投稿されました"
-      redirect_to @task
-      else 
-      flash.now[:danger] = "Taskが正常に投稿されませんでした"
-      render :new
+        flash[:success] = "Taskを投稿しました"
+        redirect_to root_url
+      else
+        @tasks = current_user.tasks.order("created_at DESC").page(params[:page])
+        flash.now[:danger] = "Taskを投稿できませんでした"
+        render "toppages/index"
       end
     end
      
@@ -42,11 +42,9 @@ class TasksController < ApplicationController
     end
     
     def destroy
-       
        @task.destroy
-       
        flash[:success] = "Taskが正常に削除されました"
-       redirect_to tasks_url
+       redirect_to root_url
     end
     
     private
@@ -57,6 +55,13 @@ class TasksController < ApplicationController
     
     def task_params
       params.require(:task).permit(:content,:status)  
+    end
+    
+    def correct_user 
+      @task = current_user.tasks.find_by(id: params[:id])    
+      unless @task
+        redirect_to root_url
+      end  
     end
     
 end
