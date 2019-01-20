2b9ab4ef715addf21458ade9ba57310ca9646243
tomoya.harada <freiheitlibre0420@icloud.com>
2019-01-20 01:43:45 +0000

    revise for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:destroy,:edit]
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:show,:update, :destroy,:edit]
     
     def index
      @tasks = Task.order(created_at: :desc).page(params[:page]).per(10)
